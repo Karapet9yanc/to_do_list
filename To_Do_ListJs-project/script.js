34cf9812f889a37c33d43070f97ef6a896e4a977
Vladislav Karapet9yanc <vl@gmail.com>
2021-11-18 13:51:48 +0300

    Code Review

--- To_Do_ListJs-project/script.js
+++ To_Do_ListJs-project/script.js
@@ -29,6 +29,8 @@ const onClickButton = async () => {
       isCheck: false
     }),
   });
+  const result = await resp.json();
+  allTasks = result.data;
   valueInput = '';
   input.value='';
   render();
@@ -43,75 +45,76 @@ const render = () => {
   while(content.firstChild){
     content.removeChild(content.firstChild);   
   };
-  allTasks.sort((a, b) => a.isCheck - b.isCheck)
-  .map((item, index) => {
-    const container = document.createElement('div');
-    container.id = `task-${index}`;
-    container.className = 'task-container';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked =  item.isCheck;
-    checkbox.onchange = () => onChangeCheckbox(index);
-    container.appendChild(checkbox);
-    const text = document.createElement('p');
-    text.innerText = item.text;
-    text.className = item.isCheck ? "text-task done-text" : "text-task";
-    container.appendChild(text);
-    const conta = document.createElement('div');
-    conta.id = 'task-4{index}';
-    conta.className = 'task-conta';
-    const imageEdit = document.createElement('img');
-    imageEdit.src = 'img/edit.png';
-    container.appendChild(imageEdit);
-    imageEdit.className = 'edit';
-    const inpSav = document.createElement('input');
-    const imageSave = document.createElement('img');
-    imageSave.src = 'img/done.png';
-    imageEdit.onclick = () => {
-      container.replaceChild(inpSav, text);
-      container.appendChild(imageSave);
-      container.replaceChild(imageSave, imageEdit);
-      inpSav.value = item.text;
-      imageDelit.onclick = () => {
+  allTasks
+    .sort((a, b) => a.isCheck - b.isCheck)
+    .map((item, index) => {
+      const container = document.createElement('div');
+      container.id = `task-${index}`;
+      container.className = 'task-container';
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked =  item.isCheck;
+      checkbox.onchange = () => onChangeCheckbox(index);
+      container.appendChild(checkbox);
+      const text = document.createElement('p');
+      text.innerText = item.text;
+      text.className = item.isCheck ? "text-task done-text" : "text-task";
+      container.appendChild(text);
+      const conta = document.createElement('div');
+      conta.id = 'task-4{index}';
+      conta.className = 'task-conta';
+      const imageEdit = document.createElement('img');
+      imageEdit.src = 'img/edit.png';
+      container.appendChild(imageEdit);
+      imageEdit.className = 'edit';
+      const inpSav = document.createElement('input');
+      const imageSave = document.createElement('img');
+      imageSave.src = 'img/done.png';
+      imageEdit.onclick = () => {
+        container.replaceChild(inpSav, text);
+        container.appendChild(imageSave);
+        container.replaceChild(imageSave, imageEdit);
+        inpSav.value = item.text;
+        imageDelit.onclick = () => {
+          render();
+        };
+      };
+      imageSave.onclick = async () => {
+        container.replaceChild(text, inpSav);
+        text.innerText = inpSav.value;
+        container.replaceChild(imageEdit, imageSave);
+        const _id = allTasks[index]._id;
+        const resp = await fetch(`http://localhost:8000/updateTask`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify ({
+            _id,
+            text : inpSav.value
+          })
+        });
+        const result = await resp.json();
+        allTasks = result.data;
+        item.text = inpSav.value;
+      };
+      const imageDelit = document.createElement('img');
+      imageDelit.src = 'img/delete.png';
+      container.appendChild(imageDelit);
+      imageDelit.className = 'delete';
+      content.appendChild(container); 
+      imageDelit.onclick = async (_id) => {
+        content.removeChild(container)
+        
+        const resp = await fetch(`http://localhost:8000/deleteTask?_id=${allTasks[index]._id}`, {
+          method: 'DELETE', 
+        });
+        const result = await resp.json();
+        allTasks = result.data;
         render();
-      }
-    }
-    imageSave.onclick = async () => {
-      container.replaceChild(text, inpSav);
-      text.innerText = inpSav.value;
-      container.replaceChild(imageEdit, imageSave);
-      const _id = allTasks[index]._id;
-      const resp = await fetch(`http://localhost:8000/updateTask`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify ({
-          _id,
-          text : inpSav.value
-        })
-      });
-      let result = await resp.json();
-      allTasks = result.data;
-      item.text = inpSav.value;
-    };
-    const imageDelit = document.createElement('img');
-    imageDelit.src = 'img/delete.png';
-    container.appendChild(imageDelit);
-    imageDelit.className = 'delete';
-    imageDelit.onclick = async (_id) => {
-      content.removeChild(container)
-      allTasks.splice(index, 0)
-      const resp = await fetch(`http://localhost:8000/deleteTask?_id=${allTasks[index]._id}`, {
-        method: 'DELETE', 
-      });
-      let result = await resp.json();
-      allTasks = result.data;
-      render();
-    };
-    content.appendChild(container); 
-  });
+      };
+    });
 };
 onChangeCheckbox = (index) => {
   allTasks[index].isCheck = !allTasks[index].isCheck;
